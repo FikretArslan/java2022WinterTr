2c636d34e3eb46de49c49c4124afb0c0a2fb5b48
FikretArslan <arslanfikret33@gmail.com>
2022-05-05 11:54:38 +0300

    05.05.2022

--- src/day26_forEachLoop_constructor/ArabaRunner.java
+++ src/day26_forEachLoop_constructor/ArabaRunner.java
@@ -3,7 +3,8 @@ package day26_forEachLoop_constructor;
 public class ArabaRunner {
 
     public static void main(String[] args) {
-        Araba        arb1     =   new        Araba(2023, "Kirmizi");
+        Araba        arb1     =   new        Araba();
         // Class adi    Obje adi    keyword     Constructor
+        System.out.println(arb1);
     }
 }
